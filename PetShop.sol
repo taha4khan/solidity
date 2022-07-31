e427d41dcd48c8a16abfa8d5cbe59f0616e950ec
Taha Khan <102149347+taha4khan@users.noreply.github.com>
2022-07-31 10:32:01 +0500

    Update PetShop.sol

--- PetShop.sol
+++ PetShop.sol
@@ -2,10 +2,16 @@
 pragma solidity ^0.8.12;
 
 contract PetShop {
-    address[20] public adopters;
+    address[20]  adopters;
+    address owner;
+    
+    // constructor(){
+    //     owner = msg.sender;
+    // }
 
     modifier isOwner(uint256 petId) {
-        require (adopters [petId] == address(0),"ERROR: Only Pet adopter can remove adoption");
+        //require (adopters [petId] == address(0),"ERROR: Only Pet adopter can remove adoption");
+        require (adopters [petId] == msg.sender,"ERROR: Only Pet adopter can remove adoption");
         _;
     }
 
@@ -23,7 +29,14 @@ contract PetShop {
         return adopters[petId]; 
     }
 
-    function removeAdoption (uint256 petId) public isOwner(petId) returns (bool){
+//     function removeAdoption (uint256 petId, address owner) public isOwner(petId) returns (bool){
+//         adopters[petId] = address(0);
+//         return true;
+//     }
+// }
+
+
+   function removeAdoption (uint256 petId) public isOwner(petId) returns (bool){
         adopters[petId] = address(0);
         return true;
     }
