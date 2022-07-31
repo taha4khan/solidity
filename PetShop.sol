b56d670e5877bb7b2077665e77a37f4613ecec8f
Taha Khan <102149347+taha4khan@users.noreply.github.com>
2022-07-31 09:14:05 +0500

    Create PetShop.sol

---
+++ PetShop.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+contract PetShop {
+    address[20] public adopters;
+
+    modifier isOwner(uint256 petId) {
+        require (adopters [petId] == address(0),"ERROR: Only Pet adopter can remove adoption");
+        _;
+    }
+
+    function adoptPet (uint256 petId) public returns (uint256){
+        require (adopters[petId] == address(0),"This is Alredy Adopted!");
+        adopters[petId] = msg.sender;
+        return petId;
+    }
+
+    function getAdopters() public view returns (address[20] memory){
+        return adopters;
+    }
+
+    function getAdopter(uint256 petId ) public view returns (address){
+        return adopters[petId]; 
+    }
+
+    function removeAdoption (uint256 petId) public isOwner(petId) returns (bool){
+        adopters[petId] = address(0);
+        return true;
+    }
+}
